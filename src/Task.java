24545d0e8a2210dfc800bd2306b748209ec9991c
Dmitry <dim4n94@yandex.ru>
2020-04-23 14:26:54 +0300

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Task {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите текст: ");
+        String text = in.nextLine();
+        System.out.println("Введённый текст: " + text);
+    }
+}
